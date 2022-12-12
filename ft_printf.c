aee991950fcd4aad1f5af233ec0d337e1ea2cb32
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-12-12 02:16:41 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -1,14 +1,28 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 
-
-void    ft_putchar(char c)
+static int	ft_intlen(int n)
 {
-    write(1, &c, 1);
+	int	ret;
+
+	ret = 1;
+	while (n >= 10)
+	{
+		ret++;
+		n = n / 10;
+	}
+	return (ret);
 }
 
-int        ft_putstr(char *s)
+int    ft_putchar(char c)
+{
+    write(1, &c, 1);
+    return (1);
+}
+
+int ft_putstr(char *s)
 {
     int ret;
 
@@ -22,30 +36,57 @@ int        ft_putstr(char *s)
     return (ret);
 }
 
-void	ft_putnbr(int n)
+int ft_putnbr(int n, int plus)
 {
+    int ret;
+
+    if (plus >= 1)
+    {
+        if (n > 0)
+        {
+            if (plus == 1)
+                ft_putchar('+');
+            if (plus == 2)
+                ft_putchar(' ');
+        }
+        ret++;
+    }
 	if (n == -2147483648)
 	{
 		ft_putstr("-2147483648");
-		return ;
+		return (12);
 	}
 	if (n < 0)
 	{
-		ft_putchar('-');
+        ft_putchar('-');
 		n = n * -1;
+        ret++;
 	}
+    ret = ft_intlen(n);
 	if (n >= 10)
 	{
-		ft_putnbr(n / 10);
+		ft_putnbr(n / 10, 0);
 		n = n % 10;
 	}
 	if (n < 10)
 		ft_putchar(n + '0');
+    return (ret);
 }
 
-int flags(va_list pointer, char *args, int ret)
+int flags(va_list pointer, char *args, int plus)
 {
+    int ret;
+
+    ret = 0;
     args++;
+    if (*args == '+' || *args == ' ')
+    {
+        args++;
+        if (*args == '+')
+            plus = 1;
+        else
+            plus = 2;
+    }  
     if (*args == 'c')
     {
         ft_putchar((char)va_arg(pointer, int));
@@ -54,7 +95,11 @@ int flags(va_list pointer, char *args, int ret)
     if (*args == 's')
         ret += ft_putstr(va_arg(pointer, char *));
     if (*args == 'i')
-        ft_putnbr((long int)va_arg(pointer, int));
+        ret += ft_putnbr((int)va_arg(pointer, int), plus);
+    if (*args == 'u')
+        ret += ft_putnbr((int)va_arg(pointer, unsigned int), plus);
+    if (*args == '%')
+        ret += ft_putchar('%');
     return (ret);
 }
 
@@ -71,8 +116,10 @@ int ft_printf(const char *args, ...)
           break ;
         if ( *args == '%')
         {
-            ret = flags(pointer, (char *)args, ret);
+            ret += flags(pointer, (char *)args, 0);
             args++;
+            if (*args == '+' || *args == ' ')
+                args++;
         }
         else
         {
@@ -81,10 +128,11 @@ int ft_printf(const char *args, ...)
         }
         args++;
     }
-    return (ret);
+    return (ret + 1);
 }
 
 int main(void)
 {
-    ft_printf("%s %i", "auisgays", 217261 + 1);
+    int f = ft_printf("%i",217261);
+    ft_printf("%i", f);
 }
